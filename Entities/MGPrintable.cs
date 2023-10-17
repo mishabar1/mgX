b7b0edf435324c81d840379c21889f12601f1897
michaelapplydesign <117651959+michaelapplydesign@users.noreply.github.com>
2023-10-17 13:08:21 +0300

    up

---
+++ Entities/MGPrintable.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace MG.Server.Entities
+{
+    public class MGPrintable
+    {
+        public override string ToString()
+        {
+            return GetType().GetProperties()
+                .Select(info => (info.Name, Value: info.GetValue(this, null) ?? "(null)"))
+                .Aggregate(
+                    new StringBuilder(),
+                    (sb, pair) => sb.AppendLine($"{pair.Name}: {pair.Value}"),
+                    sb => sb.ToString());
+        }
+    }
+}
---
+++ Entities/UserData.cs
@@ -0,0 +1,11 @@
+﻿namespace MG.Server.Entities
+{
+    public class UserData : BaseData<UserData>
+    {
+
+        public UserData():base()
+        {
+           
+        }
+    }
+}
