ac45344c75249d58b9c771faf4cefa304dd677a6
michaelapplydesign <117651959+michaelapplydesign@users.noreply.github.com>
2023-10-27 08:27:17 +0300

    up

---
+++ Client/src/app/entities/game.data.ts
@@ -0,0 +1,24 @@
+import { ItemData } from "./item.data";
+import { PlayerData } from "./player.data";
+import {AssetData} from './asset.data';
+import {find} from 'lodash';
+import {LocationData} from './location.data';
+
+export class GameData {
+
+  id!: string;
+  name?: string;
+  assets!: { [key: string]: AssetData };
+  table!: ItemData;
+  attributes: any;
+  creatorId?: string;
+  currentTurnId?: string;
+  gameStatus!: number;
+  gameType!: number;
+  players!: PlayerData[];
+  winners?: PlayerData[];
+
+  observer!:LocationData;
+
+}
+
