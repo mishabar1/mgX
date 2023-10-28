4710a771db1190279fd80d12463afcfd6f618ab3
michaelapplydesign <117651959+michaelapplydesign@users.noreply.github.com>
2023-10-28 11:11:24 +0300

    up

---
+++ Entities/LocationData.cs
@@ -0,0 +1,19 @@
+﻿namespace MG.Server.Entities
+{
+    public class LocationData: BaseData<LocationData>
+    {
+        public V3 Position { get; set; }
+        public V3 Rotation { get; set; }
+        public V3 Scale { get; set; }
+
+        public LocationData()
+        {
+            Position = new V3();
+            Rotation = new V3();
+            Scale = new V3(1);
+        }        
+
+        
+
+    }
+}
