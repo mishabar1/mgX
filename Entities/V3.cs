844359b45c608ad3f4a0d0013b6ccfc1cd19ae0b
michaelapplydesign <117651959+michaelapplydesign@users.noreply.github.com>
2023-10-29 16:24:30 +0200

    up

---
+++ Entities/V3.cs
@@ -0,0 +1,45 @@
+﻿
+using MG.Server.Services;
+
+namespace MG.Server.Entities
+{
+
+    public class V3
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public V3() : this(0, 0, 0) { }
+
+        public V3(double a) : this(a, a, a) { }
+
+        public V3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return Utils.ListProperties(this);
+        }
+
+        public void Set(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Set(V3 v3)
+        {
+            X = v3.X;
+            Y = v3.Y;
+            Z = v3.Z;
+        }
+
+
+    }
+}
