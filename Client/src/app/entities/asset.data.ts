d5de33f6a9edafa94199f5e46addb7359600a6c7
michaelapplydesign <117651959+michaelapplydesign@users.noreply.github.com>
2023-11-07 07:00:43 +0200

    up 38

---
+++ Client/src/app/entities/asset.data.ts
@@ -0,0 +1,18 @@
+import {V3} from "./V3";
+
+export class AssetData {
+  id!: string;
+  name?: string;
+
+  frontURL!: string;
+  backURL?: string;
+
+  type!:string;
+
+  scale!:V3;
+
+  // "TOKEN"; // some "box" with very small height and 2 sides - front and back
+  // "SOUND"; // mp3 sound - can be played on demand
+  // "OBJECT"; // stl, gbl or obj file to load a 3d model
+  // "TEXT"; // 3d text
+}
